d344029945f6ecee3aeb5b754fc89f948eba3057
araceliYM <168685891+araceliYM@users.noreply.github.com>
2024-08-02 12:44:09 -0600

    Added ActionEventGameObject.java via upload
    Java interface to provide common methods to game objects

---
+++ ActionEventGameObject.java
@@ -0,0 +1,9 @@
+// Purpose:          Java interface used to provide image files for rock, paper, scissors game.
+// Programmer:       Araceli YM
+// Created on:       7/28/24
+
+// CIS263: Interface used by all GameObject child classes.
+interface ActionEventGameObject
+{
+   public String getImagePath();
+}
